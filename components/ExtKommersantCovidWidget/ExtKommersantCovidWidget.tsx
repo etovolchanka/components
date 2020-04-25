0a1d0990b9631db1d1777176fc50bdeb80ab33d2
etovolchanka <etovolchanka@gmail.com>
2020-04-25 18:20:09 +0300

    Add ExtKommersantCovidWidget component

---
+++ components/ExtKommersantCovidWidget/ExtKommersantCovidWidget.tsx
@@ -0,0 +1,188 @@
+import * as React from 'react';
+import './ExtKommersantCovidWidget.scss';
+
+interface ICovidStats {
+    Date: string;
+    CreatedAt: string;
+    UpdatedAt: string | null;
+    WorldInfected: number;
+    WorldInfectedDiff: number;
+    WorldDead: number;
+    WorldDeadDiff: number;
+    WorldRecovered: number;
+    WorldRecoveredDiff: number;
+    RussiaInfected: number;
+    RussiaInfectedDiff: number;
+    RussiaDead: number;
+    RussiaDeadDiff: number;
+    RussiaRecovered: number;
+    RussiaRecoveredDiff: number;
+    MoscowInfected: number;
+    MoscowInfectedDiff: number;
+    MoscowDead: number;
+    MoscowDeadDiff: number;
+    MoscowRecovered: number;
+    MoscowRecoveredDiff: number;
+}
+
+interface ICovidArealStats {
+    Infected: number;
+    InfectedDiff: number;
+    Dead: number;
+    DeadDiff: number;
+    Recovered: number;
+    RecoveredDiff: number;
+}
+
+interface ICovidWidgetState {
+    allStats: ICovidStats | null;
+    activeTab: Tab;
+}
+
+interface ICovidWidgetProps {
+    ['link-to-details']: string;
+    ['data-source-url']: string;
+}
+
+enum Tab {
+    World = 'World',
+    Russia = 'Russia',
+    Moscow = 'Moscow',
+}
+
+const separateThousands = (target: number): string => target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '&nbsp;');
+
+export class ExtKommersantCovidWidget extends React.Component<ICovidWidgetProps, ICovidWidgetState> {
+    private get lastUpdate(): string | null {
+        const { allStats } = this.state as ICovidWidgetState;
+        if (allStats) {
+            return allStats.UpdatedAt || allStats.CreatedAt;
+        }
+        return null;
+    }
+
+    public state = {
+        allStats: null,
+        activeTab: Tab.Russia
+    }
+
+    public async componentDidMount(): Promise<void> {
+        try {
+            await this.fetchStats();
+        } catch (e) {
+            console.error(e.message);
+        }
+    }
+
+    public shouldComponentUpdate(nextProps: ICovidWidgetProps, nextState: ICovidWidgetState): boolean {
+        return this.state.activeTab !== nextState.activeTab;
+    }
+
+    public render(): React.ReactNode {
+        const { allStats } = this.state;
+        if (allStats) {
+            const tabStats = this.getStatsForActiveTab(allStats);
+            const lastUpdateDate = this.getLastUpdateDate();
+            const lastUpdateTime = this.getLastUpdateTime();
+            return (
+                <div className="covid-stats-widget-wrapper">
+                    <div className="covid-stats-widget">
+                        <div className="covid-stats-widget__header">
+                            <div className="covid-stats-widget__virus-icon" />
+                            <div className="covid-stats-widget__caption">Статистика по коронавирусу COVID-19</div>
+                        </div>
+                        <div className="covid-stats-widget__tabs">
+                            <div className={`covid-stats-widget__tab ${this.state.activeTab === Tab.World ? 'covid-stats-widget__tab_active' : ''}`} data-tab={Tab.World} onClick={this.handleTabSwitch}>Мир</div>
+                            <div className={`covid-stats-widget__tab ${this.state.activeTab === Tab.Russia ? 'covid-stats-widget__tab_active' : ''}`} data-tab={Tab.Russia} onClick={this.handleTabSwitch}>Россия</div>
+                            <div className={`covid-stats-widget__tab ${this.state.activeTab === Tab.Moscow ? 'covid-stats-widget__tab_active' : ''}`} data-tab={Tab.Moscow} onClick={this.handleTabSwitch}>Москва</div>
+                        </div>
+                        <div className="covid-stats-widget__body">
+                            <div className="covid-stats-widget__column covid-stats-widget__column_infected">
+                                <div className="covid-stats-widget__column-header">Заболели</div>
+                                <div className="covid-stats-widget__column-content">
+                                    <div className="covid-stats-widget__column-content-header">{separateThousands(tabStats.Infected)}</div>
+                                    <div>+{separateThousands(tabStats.InfectedDiff)}</div>
+                                    <div>(за сутки)</div>
+                                </div>
+                            </div>
+                            <div className="covid-stats-widget__column covid-stats-widget__column_recovered">
+                                <div className="covid-stats-widget__column-header">Выздоровели</div>
+                                <div className="covid-stats-widget__column-content">
+                                    <div className="covid-stats-widget__column-content-header">{separateThousands(tabStats.Recovered)}</div>
+                                    <div>+{separateThousands(tabStats.RecoveredDiff)}</div>
+                                    <div>(за сутки)</div>
+                                </div>
+                            </div>
+                            <div className="covid-stats-widget__column covid-stats-widget__column_dead">
+                                <div className="covid-stats-widget__column-header">Умерли</div>
+                                <div className="covid-stats-widget__column-content">
+                                    <div className="covid-stats-widget__column-content-header">{separateThousands(tabStats.Dead)}</div>
+                                    <div>+{separateThousands(tabStats.DeadDiff)}</div>
+                                    <div>(за сутки)</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="covid-stats-widget-extra">
+                        {lastUpdateDate && lastUpdateTime ? (
+                            <div className="covid-stats-widget-extra__updated-at">
+                                Данные обновлены {lastUpdateDate} в {lastUpdateTime} по московскому времени
+                            </div>
+                        ) : ''}
+                        <div className="covid-stats-widget-extra__details-link-contaier">
+                            <a className="covid-stats-widget-extra__details-link" href={this.props['link-to-details']}>Вся статистика, графики и карты</a>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        return null;
+    }
+
+    private async fetchStats(): Promise<void> {
+        if (typeof window === 'undefined') {
+            throw new Error('Window is not defined');
+        } else {
+            const response = await (await fetch(this.props['data-source-url'])).json();
+            const [allStats] = response.data;
+            this.setState({
+                allStats
+            });
+        }
+    }
+
+    private getStatsForActiveTab(allStats: ICovidStats): ICovidArealStats {
+        const { activeTab } = this.state;
+        return {
+            Infected: allStats[`${activeTab}Infected`],
+            InfectedDiff: allStats[`${activeTab}InfectedDiff`],
+            Dead: allStats[`${activeTab}Dead`],
+            DeadDiff: allStats[`${activeTab}DeadDiff`],
+            Recovered: allStats[`${activeTab}Recovered`],
+            RecoveredDiff: allStats[`${activeTab}RecoveredDiff`]
+        };
+    }
+
+    private getLastUpdateDate(): string | null {
+        return this.lastUpdate ? new Date(this.lastUpdate).toLocaleDateString() : null;
+    }
+
+    private getLastUpdateTime(): string | null {
+        const convertOptions: {[index: string]: string} = { hour: '2-digit', minute: '2-digit' };
+        return this.lastUpdate ? new Date(this.lastUpdate).toLocaleTimeString([], convertOptions) : null;
+    }
+
+    private handleTabSwitch(event: React.MouseEvent<HTMLDivElement>): void {
+        const target = event.target as HTMLDivElement;
+        const tab = target.getAttribute('data-tab');
+        if (tab) {
+            this.switchTabTo(tab as Tab);
+        }
+    }
+
+    private switchTabTo(newTab: Tab): void {
+        this.setState({
+            activeTab: newTab
+        });
+    }
+}
