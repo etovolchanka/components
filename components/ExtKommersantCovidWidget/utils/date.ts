b29073c5bb07c9b2b013738d79687a98b009ea98
etovolchanka <etovolchanka@gmail.com>
2020-04-26 16:50:06 +0300

    Replace exported arrow functions with simple ones

--- components/ExtKommersantCovidWidget/utils/date.ts
+++ components/ExtKommersantCovidWidget/utils/date.ts
@@ -1,8 +1,10 @@
-const getLocaleDate = (ISOString: string): string => new Date(ISOString).toLocaleDateString();
+function getLocaleDate(ISOString: string): string {
+    return new Date(ISOString).toLocaleDateString();
+}
 
-const getLocaleTime = (ISOString: string): string => {
+function getLocaleTime(ISOString: string): string {
     const convertOptions: {[index: string]: string} = { hour: '2-digit', minute: '2-digit' };
     return new Date(ISOString).toLocaleTimeString([], convertOptions);
-};
+}
 
 export { getLocaleDate, getLocaleTime };
--- components/ExtKommersantCovidWidget/utils/values.ts
+++ components/ExtKommersantCovidWidget/utils/values.ts
@@ -1,1 +1,3 @@
-export const separateThousands = (target: number): string => target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '\u00a0');
+export function separateThousands(target: number): string {
+    return target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '\u00a0');
+}
