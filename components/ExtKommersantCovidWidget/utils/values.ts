cb3ad15c5e67f85665a4fb3c9ae83197a3b66b86
etovolchanka <etovolchanka@gmail.com>
2020-04-28 15:33:35 +0300

    Made function less obscure

--- components/ExtKommersantCovidWidget/utils/values.ts
+++ components/ExtKommersantCovidWidget/utils/values.ts
@@ -1,3 +1,5 @@
 export function separateThousands(target: number): string {
-    return target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '\u00a0');
+    const nbsp = '\u00a0';
+    const threeDigitsCondition = /\B(?=(\d{3})+(?!\d))/g;
+    return target.toString().replace(threeDigitsCondition, nbsp);
 }
